60342dd522f914f8b52b7b1b3a076a333b2cdf55
Michael Elsdoerfer <michael@elsdoerfer.com>
2007-03-17 10:38:00 +0000

    some minor changes to VersionInfo system - moved some helper functions from Core.pas to VersionInfo.pas, where they fit better. The includes now only contain the consts, not the whole unit content (with impl and  interface sections), as before.

--- src/Core.pas
+++ src/Core.pas
@@ -3,52 +3,23 @@ unit Core;
 interface
 
 uses
-  SysUtils, VersionInfo;
+  VersionInfo;
 
 const
   Appname = 'Wormtail';
   function AppnameWithVersion: string;
   function AppVersion: string;
 
-function MakeAppVersion(): string;
-
 implementation
-
-{$WARNINGS off}  // Prevent "Comparison always evaluates to True" for constant comparisons
-function MakeAppVersion(): string;
-begin
-  Result := '';
-
-  // Start off with major version
-  if (WORMTAIL_VERSION_MAJOR<>-1) then
-  begin
-    Result := Result+IntToStr(WORMTAIL_VERSION_MAJOR);
-    // No minor version without a major on
-    if (WORMTAIL_VERSION_MINOR<>-1) then
-      Result := Result+'.'+IntToStr(WORMTAIL_VERSION_MINOR);
-  end;
-
-  // Ident
-  if (WORMTAIL_VERSION_IDENT<>'') then
-  begin
-    if Result <> '' then Result := Result+'-';
-    Result := Result+WORMTAIL_VERSION_IDENT;
-  end;
-
-  // Add build number
-  if (WORMTAIL_VERSION_BUILD<>-1) then
-    Result := Result+'.'+IntToStr(WORMTAIL_VERSION_BUILD);
-end;
-{$WARNINGS on}
 
 function AppVersion: string;
 begin
-  Result := MakeAppVersion;  
+  Result := MakeVersionString(vsfFull);  
 end;
 
 function AppnameWithVersion: string;
 begin
-  Result := Appname+' '+AppVersion;
+  Result := Appname+' '+MakeVersionString(vsfShort);
 end;
 
 end.
--- src/VersionInfo.pas
+++ src/VersionInfo.pas
@@ -1,6 +1,11 @@
 unit VersionInfo;
 
-// Depending on build mode, retrieve the content of this unit from an include.
+// Depending on build mode, retrieve the actual version info from an include.
+
+interface
+
+uses
+  SysUtils;
 
 {
     What we expect from the include file:
@@ -21,5 +26,43 @@ unit VersionInfo;
 {$ELSE}
   {$INCLUDE VersionInfoIDE.inc}
 {$ENDIF}
+
+type
+  TVersionStringFormat = (
+    vsfFull,     // major/minor/ident/build
+    vsfLong,     // major/minor/ident
+    vsfShort     // major/minor
+  );
+
+function MakeVersionString(Format: TVersionStringFormat): string;
+
+implementation
+
+{$WARNINGS off}  // Prevent "Comparison always evaluates to True" for constant comparisons
+function MakeVersionString(Format: TVersionStringFormat): string;
+begin
+  Result := '';
+
+  // Start off with major version
+  if (WORMTAIL_VERSION_MAJOR<>-1) then
+  begin
+    Result := Result+IntToStr(WORMTAIL_VERSION_MAJOR);
+    // No minor version without a major on
+    if (WORMTAIL_VERSION_MINOR<>-1) then
+      Result := Result+'.'+IntToStr(WORMTAIL_VERSION_MINOR);
+  end;
+
+  // Ident
+  if (WORMTAIL_VERSION_IDENT<>'') then
+  begin
+    if Result <> '' then Result := Result+'-';
+    Result := Result+WORMTAIL_VERSION_IDENT;
+  end;
+
+  // Add build number
+  if (WORMTAIL_VERSION_BUILD<>-1) then
+    Result := Result+'.'+IntToStr(WORMTAIL_VERSION_BUILD);
+end;
+{$WARNINGS on}
 
 end.
